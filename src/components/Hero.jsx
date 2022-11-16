462b2a24afc52aa2fcb24ea073f702f1b2aed0cb
Jenil Patel <98316039+jenil-patel97@users.noreply.github.com>
2022-11-16 11:08:39 +0530

    fix : Hero with framer motion & varients

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,28 +1,48 @@
 import React from "react";
 import Header from "./Header";
+import { motion } from "framer-motion";
+import { fadeIn, staggerContainer } from "../variants";
 
 const Hero = () => {
   return (
-    <section className="min-h-[980px] bg-hero bg-cover bg-right">
+    <section className="min-h-[750px] bg-hero bg-cover bg-right">
       {/* header */}
       <Header />
       <div className="container mx-auto lg:min-h-[700px] min-h-[850px] flex justify-center items-center">
         {/* text */}
-        <div className="text-center flex flex-col items-center">
+        <motion.div
+          variants={staggerContainer(0.3, 1)}
+          initial="hidden"
+          whileInView={"show"}
+          className="text-center flex flex-col items-center"
+        >
           {/* pretitle */}
-          <div className="text-white text-[24px] lg:text-[28px] font-primary italic lg:font-medium mb-4 lg:mb-2">
+          <motion.div
+            variants={fadeIn("down", "tween", 0.2, 1.1)}
+            className="text-white text-[24px] lg:text-[28px] font-primary italic lg:font-medium mb-4 lg:mb-2 cursor-default"
+          >
             Nothing brings together like
-          </div>
+          </motion.div>
           {/* title */}
-          <h1 className="h1 mb-5">Bistro Cafe</h1>
+          <motion.h1
+            variants={fadeIn("down", "tween", 0.3, 1.1)}
+            className="h1 mb-5 cursor-default"
+          >
+            Bistro Cafe
+          </motion.h1>
           {/* subtitle */}
-          <p className="text-white max-w-[540px] font-primary mb-8">
+          <motion.p
+            variants={fadeIn("down", "tween", 0.4, 1.1)}
+            className="text-white max-w-[540px] font-primary italic mb-8 cursor-default"
+          >
             Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
             sint. Velit officia consequat duis enim velit mollit .{" "}
-          </p>
+          </motion.p>
           {/* button */}
-          <button className="btn">Find out more</button>
-        </div>
+          <motion.div variants={fadeIn("down", "tween", 0.5, 1.1)}>
+            <button className="btn font-primary">Find more</button>
+          </motion.div>
+        </motion.div>
       </div>
     </section>
   );
