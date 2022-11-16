ef967e88d8818e1738cc606a99b479f4f33862c5
Jenil Patel <98316039+jenil-patel97@users.noreply.github.com>
2022-11-16 10:12:34 +0530

    feat : index.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,44 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      primary: "Playfair Display",
+      secondary: "Lato",
+    },
+    container: {
+      padding: {
+        DEFAULT: "10px",
+        // lg: '0',
+      },
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1200px",
+    },
+    extend: {
+      colors: {
+        body: "#e5e5e5",
+        dark: "#0a0909",
+        grey: {
+          DEFAULT: "#777876",
+          100: "#e4e4e3",
+        },
+        accent: {
+          DEFAULT: "#fe7634",
+          hover: "#F55304",
+        },
+      },
+      backgroundImage: {
+        pattern: "url('assets/body-bg-pattern.png')",
+        hero: "url('assets/hero/bg.png')",
+        menu: "url('assets/menu/bg.png')",
+        testimonial: "url('assets/testimonial/bg.png')",
+        newsletter: "url('assets/newsletter/bg.png')",
+        footer: "url('assets/footer/bg.png')",
+      },
+    },
   },
   plugins: [],
 };
