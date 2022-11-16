6b372b23b2209251ea0e2ed9f26e87064b3893a0
Jenil Patel <98316039+jenil-patel97@users.noreply.github.com>
2022-11-16 13:10:45 +0530

    feat : Nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const Nav = () => {
   return (
-    <div>Nav</div>
-  )
-}
+    <nav className="w-full h-full">
+      <ul className="lg:py-3 py-12">
+        <li className="h-full flex flex-col justify-center items-center gap-y-8 lg:gap-y-6">
+          <a className="text-xl capitalize font-primary italic hover:text-dark transition-all duration-300" href="/">Home</a>
+          <a className="text-xl capitalize font-primary italic hover:text-dark transition-all duration-300" href="/">about</a>
+          <a className="text-xl capitalize font-primary italic hover:text-dark transition-all duration-300" href="/">menu</a>
+          <a className="text-xl capitalize font-primary italic hover:text-dark transition-all duration-300" href="/">team</a>
+          <a className="text-xl capitalize font-primary italic hover:text-dark transition-all duration-300" href="/">book a table</a>
+          <a className="text-xl capitalize font-primary italic hover:text-dark transition-all duration-300" href="/">contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
-export default Nav
+export default Nav;
